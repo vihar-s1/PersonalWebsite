ca66c4b89bb15ea5e2fc018c8348456d9da4b476
Vihar <vihar2121@gmail.com>
2023-05-13 23:49:58 +0530

    Finalized transparent contact me form
    Validations were enabled only after submitting contact-me unsuccessfully once because being called inside submit handler.
    Instead, now the validation function is defined inside use-effect and hence contact-me form cannot be submitted unless all checks are passed even on first attempt.

--- frontend/src/components/ContactMe.js
+++ frontend/src/components/ContactMe.js
@@ -1,119 +1,118 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { SocialIcon } from "react-social-icons";
 import { themeContext } from '../context/ThemeContext';
 
+import SocialDetails from "../json/SocialDetails.json";
+
 const ICON_DIM="30px";
+const TEXT_THEME = "text-dark";
+const INPUT_CLASS = "bg-light bg-opacity-50 shadow-lg";
+
 
 const ContactMe = () => {
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    useEffect(() => {
         'use strict'
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         const forms = document.querySelectorAll('.needs-validation')
-
         // Loop over them and prevent submission
         Array.from(forms).forEach(form => {
+            
             form.addEventListener('submit', event => {
                 if (!form.checkValidity()) {
                     event.preventDefault()
                     event.stopPropagation()
                 }
-
+    
                 form.classList.add('was-validated')
             }, false)
         })
-        alert(document.getElementById("message").value);
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("Message Sent !");
     }
-    
-    const { theme, textTheme, background } = useContext(themeContext);
 
     return (    
-        <div className={`d-flex justify-content-center flex-wrap bg-${theme} text-${textTheme}`} style={{fontFamily: "kalam"}} >
+        <div 
+            className={`d-flex justify-content-center flex-wrap ${TEXT_THEME}`} 
+            style={{background: "url(contactme.webp) no-repeat center center/cover", fontFamily: "kalam"}}
+        >
             
-            <div className="col-md-5 p-4 m-3 shadow-lg">
+            <div className="col-md-5 p-4 m-3 bg-light bg-opacity-10 shadow-lg">
                 <h1>My Contact Details</h1>
-                <ul className="list-group m-3" >   
-                    <div className={`list-group-item d-flex shadow-sm flex-wrap align-items-center bg-${theme} text-${textTheme}`}>
-                        <SocialIcon className='m-2 rounded-circle shadow-lg bg-light' style={{width: ICON_DIM, height: ICON_DIM}} url={process.env.REACT_APP_MAIL_LINK}/>
-                        <div>
-                            <div className="h4">Mail:</div>
-                            <div className='h5'>vihar2121@gmail.com</div>
-                        </div>
-                    </div>
-
-                    <div className={`list-group-item d-flex shadow-sm flex-wrap align-items-center bg-${theme} text-${textTheme}`}>
-                        <SocialIcon className='m-2 rounded-circle shadow-lg bg-light' style={{width: ICON_DIM, height: ICON_DIM}} url={process.env.REACT_APP_GITHUB_LINK}/>
-                        <div>
-                            <div className="h4">GitHub:</div>
-                            <div className='h5'>vihar-s1</div>
-                        </div>
-                    </div>
-
-                    <div className={`list-group-item d-flex shadow-sm flex-wrap align-items-center bg-${theme} text-${textTheme}`}>
-                        <SocialIcon className='m-2 rounded-circle shadow-lg bg-light' style={{width: ICON_DIM, height: ICON_DIM}} url={process.env.REACT_APP_LINKEDIN_LINK}/>
-                        <div>
-                            <div className="h4">Linked-In:</div>
-                            <div className="h5">vihar Shah</div>
-                        </div>
-                    </div>
-
-                    <div className={`list-group-item d-flex shadow-sm flex-wrap align-items-center bg-${theme} text-${textTheme}`}>
-                        <SocialIcon className='m-2 rounded-circle shadow-lg bg-light' style={{width: ICON_DIM, height: ICON_DIM}} url={process.env.REACT_APP_WHATSAPP_LINK} network="whatsapp"/>
-                        <div>
-                            <div className="h4">WhatsApp:</div>
-                            <div className="h5">+91 9377402438</div>
-                        </div>
-                    </div>
+                <ul className="list-group m-3" > 
+                    {
+                        SocialDetails.map((socialLink, index) => {
+                            return (
+                                <div key={index} 
+                                    className={`list-group-item d-flex  shadow-lg flex-wrap align-items-center bg-light bg-opacity-25 ${TEXT_THEME}`}
+                                >
+                                    <SocialIcon 
+                                        className='m-2 rounded-circle shadow-lg bg-light' 
+                                        style={{width: ICON_DIM, height: ICON_DIM}} 
+                                        url={socialLink.url} 
+                                        network={socialLink.network}
+                                    />
+                                    <div>
+                                        <div className="h4">{socialLink.field}:</div>
+                                        <div className='h5'>{socialLink.value}</div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </ul>
             </div>
 
-            <div className='col-md-6 p-4 m-3 shadow-lg' >
+            <div className='col-md-6 p-4 m-3 bg-light bg-opacity-10 shadow-lg' >
                 <h1>Send Me a Message !</h1>
-                <form className={`row g-3 needs-validation text-${textTheme}`} noValidate onSubmit={handleSubmit}>
-                    <div className="col-md-7">
+                <form className={`row g-3 needs-validation ${TEXT_THEME}`} noValidate onSubmit={handleSubmit}>
+                    <div className="col-md-7 pb-3">
                         <label htmlFor="firstName" className="form-label h5">Your Name</label>
-                        <input type="text" className={`shadow-2 form-control bg-${theme} text-${textTheme}`} id="firstName" required />
-                        <div className="invalid-feedback">
+                        <input type="text" className={`form-control ${INPUT_CLASS} ${TEXT_THEME}`} id="firstName" required/>
+                        <div className="invalid-feedback" style={{width: "fit-content"}}>
                             Please enter your Name!
                         </div>
                     </div>
-                    <div className="col-md-5">
-                        <label htmlFor="email" className="form-label h4">Email</label>
-                        <input type="email" className={`shadow-2 form-control bg-${theme} text-${textTheme}`} id="email" autoComplete='yes' required />
-                        <div className="invalid-feedback">
+                    <div className="col-md-5 pb-3">
+                        <label htmlFor="email" className="form-label h5">Email</label>
+                        <input type="email" className={`form-control ${INPUT_CLASS} ${TEXT_THEME}`} id="email" autoComplete='yes' required/>
+                        <div className="invalid-feedback"  style={{width: "fit-content"}}>
                             Please enter valid Email!
                         </div>
                     </div>
-                    <div className="col-md-4">
-                        <label htmlFor="groupType" className="form-label h5">Are You An:</label>
+                    <div className="col-md-4 pb-3">
+                        <label htmlFor="groupType" className="form-label h5">Are You A/An:</label>
                         <div>
-                            <select id="groupType" className={`shadow-2 form-select bg-${theme} text-${textTheme}`}>
+                            <select id="groupType" className={`form-select ${INPUT_CLASS} ${TEXT_THEME}`}>
                                 <option value="Individual">Individual</option>
                                 <option value="Group">Group</option>
                                 <option value="Company">Company</option>
                             </select>
                         </div>
                     </div>
-                    <div className="col-md-8">
+                    <div className="col-md-8 pb-3">
                         <label htmlFor="subject" className="form-label h5">Subject</label>
-                        <input type="text" className={`shadow-2 form-control bg-${theme} text-${textTheme}`} id="subject" required />
-                        <div className="invalid-feedback">
+                        <input type="text" className={`form-control ${INPUT_CLASS} ${TEXT_THEME}`} id="subject" required/>
+                        <div className="invalid-feedback" style={{width: "fit-content"}}>
                             Enter the Reason to Contact Here.
                         </div>
                     </div>
-                    <div className="col-md-12">
+                    <div className="col-md-12 pb-3">
                         <label htmlFor="message" className="form-label h5">Message</label>
-                        <textarea id="message" className={`shadow-2 form-control bg-${theme} text-${textTheme}`} rows="5" required/>
-                        <div className="invalid-feedback">
+                        <textarea id="message" className={`form-control ${INPUT_CLASS} ${TEXT_THEME}`} rows="5" required/>
+                        <div className="invalid-feedback" style={{width: "fit-content"}}>
                             Describe the Reason to Contact Here.
                         </div>
                     </div>
-                    <div className="col-12 text-center">
-                        <button className={`btn btn-outline-${theme == "dark" ? "info" : "primary"} shadow-5-strong`} type="submit"><h5>Send the Message</h5></button>
+                    <div className="col-12 pb-3 text-center">
+                        <button className="btn btn-light bg-light bg-opacity-75 shadow-lg" type="submit"><h5>Send the Message</h5></button>
                     </div>
                 </form>
             </div>
+            
         </div>
     )
 };
