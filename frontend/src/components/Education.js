07d8ed96514745b441465c4e43e270968771dfb2
Vihar <vihar2121@gmail.com>
2023-05-18 00:29:07 +0530

    Basic Education Page with details

--- frontend/src/components/Education.js
+++ frontend/src/components/Education.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { themeContext } from "../context/ThemeContext";
 
-const Education = () => {
+import EducationDetails from "../json/EducationDetails.json"
+
+const DegreeCard = ({ degree }) => {
     return (
-        <div>
-            Education
+        <div className="card m-3 bg-light bg-opacity-25">
+            <div className="card-body d-flex justify-content-between">
+                <div>
+                    <h4>{degree.title}</h4>
+                    <p className="mb-0">
+                        {degree.institute} (Batch {degree.passingYear})
+                    </p>
+                </div>
+                <div className="align-self-center">
+                    <div className="h3 mb-0">{degree.score}/{degree.total}</div>
+                </div>
+            </div>
         </div>
     )
+}
+
+const Education = () => {
+
+    const { theme, textTheme } = useContext(themeContext);
+
+    return (
+        <div className={`text-${textTheme} text-bg-${theme}`} style={{fontFamily: "kalam"}}>
+            <h1 className="text-center">My Educational Journey</h1>
+            <div className="py-3">
+                {
+                    EducationDetails !== undefined &&
+                    EducationDetails.map((edu, index) => {
+                        return (
+                            <DegreeCard key={index} degree={edu}/>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
 };
 
 export default Education;
--- frontend/src/components/Introduction.js
+++ frontend/src/components/Introduction.js
@@ -7,8 +7,8 @@ const Introduction = () => {
         <div style={{background: "url(introduction.webp) no-repeat center center/cover"}}>
             <div className="container text-light py-5" style={{fontFamily: "satisfy"}}>
                 <div className="p-5 text-center rounded-3">
-                    <p className="display-4 flash">I am Vihar Shah</p>
-                    <p className="col-lg-8 mx-auto fs-3 flash">
+                    <p className="display-4 fst-italic">I am Vihar Shah</p>
+                    <p className="col-lg-8 mx-auto fs-3">
                         Welcome to my website. See my projects, certificates, and resume, or just contact me through the contact me form!
                     </p>
                 </div>
