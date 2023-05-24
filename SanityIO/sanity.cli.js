00c598e21e7634f1eb058bc72e2d3d6e5265e9ac
Vihar <vihar2121@gmail.com>
2023-05-25 00:39:23 +0530

    Resolving issues and Displaying blogs from SanityIO

--- SanityIO/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import { defineCliConfig } from 'sanity/cli';
-
-export default defineCliConfig({
-  	api: {
-		projectId: 'gse4qw7e',
-		dataset: 'production'
-  	}
-});
--- SanityIO/schemas/index.js
+++
@@ -1,7 +0,0 @@
-import blog from "./blog";
-import author from "./author";
-
-export const schemaTypes = [
-    blog,
-    author,
-]
